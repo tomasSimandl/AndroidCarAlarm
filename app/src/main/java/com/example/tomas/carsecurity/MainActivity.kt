a1107b4c888f0fe50d39f09e38c182a2ae55cf82
Tomáš Šimandl <tomas.simandl@gmail.com>
2018-08-29 15:37:36 +0200

    refactor + memory released

--- app/src/main/java/com/example/tomas/carsecurity/MainActivity.kt
+++ app/src/main/java/com/example/tomas/carsecurity/MainActivity.kt
@@ -1,21 +1,17 @@
 package com.example.tomas.carsecurity
 
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
-import android.content.Intent
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import com.example.tomas.carsecurity.utils.Alarm
-import kotlinx.android.synthetic.main.activity_main.*
-import android.support.v4.content.LocalBroadcastManager
-import android.content.IntentFilter
 import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
 import android.support.v4.content.ContextCompat
+import android.support.v4.content.LocalBroadcastManager
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
-import com.example.tomas.carsecurity.utils.Tracker
 import com.example.tomas.carsecurity.utils.UtilsEnum
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,19 +24,12 @@ class MainActivity : AppCompatActivity() {
             val utilName = intent.getStringExtra(getString(R.string.key_util_name))
             val utilEnabled = intent.getBooleanExtra(getString(R.string.key_util_activated), false)
 
-            console.text = "Received broadcast from: \n" + utilName + "\nwith request: " + utilEnabled + "\n" + console.text
-
             when (utilName) {
-
                 UtilsEnum.Alarm.name -> changeColor(actionAlarm, utilEnabled)
                 UtilsEnum.Tracker.name -> changeColor(actionTracker, utilEnabled)
             }
-
-
         }
     }
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,7 +37,6 @@ class MainActivity : AppCompatActivity() {
 
         actionAlarm.setOnClickListener {
             sendIntentSwitchUtil(UtilsEnum.Alarm)
-
         }
 
         actionTracker.setOnClickListener{
@@ -56,21 +44,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onStart() {
-        super.onStart()
-        LocalBroadcastManager.getInstance(this).registerReceiver(receiver,
-                IntentFilter(getString(R.string.utils_ui_update))
-        )
-    }
-
     override fun onResume() {
         super.onResume()
+
+        val broadcastManager = LocalBroadcastManager.getInstance(this)
+        broadcastManager.registerReceiver(receiver, IntentFilter(getString(R.string.utils_ui_update)))
+
         sendIntent(MainService.Actions.ActionStatusUI.name)
     }
 
-    override fun onStop() {
+    override fun onPause() {
         LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver)
-        super.onStop()
+        super.onPause()
     }
 
 
@@ -81,7 +66,6 @@ class MainActivity : AppCompatActivity() {
             button.setBackgroundColor(ContextCompat.getColor(this, R.color.disabled))
         }
     }
-
 
     private fun sendIntent(action: String){
         val intent = Intent(applicationContext, MainService::class.java)
@@ -94,47 +78,5 @@ class MainActivity : AppCompatActivity() {
         intent.action = MainService.Actions.ActionSwitchUtil.name
         intent.putExtra("util", util)
         startService(intent)
-    }
-
-    private val deviceAdr:String = "00:11:67:63:45:CE"
-    private fun bluetoothButtonAction(){
-        val adapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-        if(!adapter.isEnabled) adapter.enable()
-
-        console.append("""size: ${adapter.bondedDevices.size}""")
-        console.append("\n")
-        println(adapter.bondedDevices.size)
-
-        var device: BluetoothDevice = adapter.getRemoteDevice("00:1F:20:A8:D1:3F")
-
-
-        adapter.bondedDevices.forEach {
-
-            console.append("""$it""")
-            console.append("\n")
-            println(it)
-
-            if (it.address == deviceAdr) {
-                device = it
-                console.append("TADA\n")
-                println("TADA")
-            }
-        }
-
-        adapter.startDiscovery()
-
-//        if(device != null){
-//
-//
-//            device!!.createRfcommSocketToServiceRecord(UUID.randomUUID())
-//
-//
-//            println(device!!.createBond())
-//
-//            console.append("\n")
-//
-//        }
-
-
     }
 }
