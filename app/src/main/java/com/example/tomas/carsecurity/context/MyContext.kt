d324c1acb7340e652fab4f13db156ea0b2d547c1
Tomáš Šimandl <tomas.simandl@gmail.com>
2018-09-21 11:07:56 +0200

    removed unused Observable from context

--- app/src/main/java/com/example/tomas/carsecurity/context/MyContext.kt
+++ app/src/main/java/com/example/tomas/carsecurity/context/MyContext.kt
@@ -3,22 +3,15 @@ package com.example.tomas.carsecurity.context
 import android.arch.persistence.room.Room
 import android.content.Context
 import android.os.Looper
-import com.example.tomas.carsecurity.R
 import com.example.tomas.carsecurity.storage.AppDatabase
-import java.util.*
 
-class MyContext(val appContext: Context, val mainServiceThreadLooper: Looper) : Observable() {
+class MyContext(val appContext: Context, val mainServiceThreadLooper: Looper) {
 
     val database = Room.inMemoryDatabaseBuilder(appContext, AppDatabase::class.java).build()
 
     val sensorContext = SensorContext(appContext)
     val utilsContext = UtilsContext(appContext)
     val communicationContext = CommunicationContext(appContext)
-
-    fun updateContext(){
-        setChanged()
-        notifyObservers()
-    }
 
     fun destroy(){
         database.close()
