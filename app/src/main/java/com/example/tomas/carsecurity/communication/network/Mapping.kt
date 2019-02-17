5a9ac2fdddf9b72cab95bb1b1f765b98607aa022
Tomáš Šimandl <tomas.simandl@gmail.com>
2019-02-17 12:40:51 +0100

    bug-fix bad url format

--- app/src/main/java/com/example/tomas/carsecurity/communication/network/Mapping.kt
+++ app/src/main/java/com/example/tomas/carsecurity/communication/network/Mapping.kt
@@ -1,7 +1,7 @@
 package com.example.tomas.carsecurity.communication.network
 
 object Mapping {
-    const val ROUTE_URL = "route/"
-    const val EVENT_URL = "event/"
-    const val LOCATION_URL = "position/"
+    const val ROUTE_URL = "route"
+    const val EVENT_URL = "event"
+    const val LOCATION_URL = "position"
 }
